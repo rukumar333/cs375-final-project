cf054f0f083d0439bdcb5640153c58b255487457
William Jagels <will.jagels@gmail.com>
2016-12-20 19:58:37 -0500

    Improve point class

--- Point.hpp
+++ Point.hpp
@@ -5,35 +5,89 @@
 #include <cmath>
 #include <initializer_list>
 #include <iostream>
+#include <stdexcept>
 
-template <size_t D>
-struct Point;
+template <size_t D, typename T>
+class Point;
 
-template <size_t D>
-struct Point {
-  Point(std::initializer_list<double> v) {
-    size_t i = 0;
-    for (auto e : v) {
-      coordinates_[i++] = e;
-    }
+/*
+ * Point of dimension D and coordinate type T.
+ * Coordinate type defaults to double.
+ */
+template <size_t D, typename T = double>
+class Point {
+ public:
+  /*
+   * Create a point with default constructed coordinates.
+   * Is disabled if T is not default constructible.
+   */
+  template <typename = typename std::enable_if<
+                std::is_default_constructible<T>::value>::type>
+  Point() : coordinates_{} {
+    coordinates_.fill(T{});
   }
 
-  explicit Point(const std::array<double, D>& arr) : coordinates_{arr} {}
+  Point(const std::initializer_list<T>& v) {
+    if (v.size() != D)
+      throw std::length_error{"Initializer list is incorrectly sized"};
+    std::copy(v.begin(), v.end(), coordinates_.begin());
+  }
 
-  double operator[](size_t dimension) const { return coordinates_[dimension]; }
+  explicit Point(const std::array<T, D>& arr) : coordinates_{arr} {}
+
+  /*
+   * No bounds check, returns by value
+   */
+  T operator[](size_t dimension) { return coordinates_[dimension]; }
+
+  const T operator[](size_t dimension) const { return coordinates_[dimension]; }
+
+  /*
+   * Bounds checked, returns reference
+   */
+  T& at(size_t dimension) { return coordinates_.at(dimension); }
+
+  const T& at(size_t dimension) const { return coordinates_.at(dimension); }
+
   Point<D>& operator=(const Point& other) {
     this->coordinates_ = other.coordinates_;
     return *this;
   }
-  std::array<double, D> coordinates_;
+
+  /*
+   * Projects the point orthogonally onto a new point of dimension DIMEN_TO.
+   * If DIMEN_TO is larger than D, the remainder of the resulting Point will
+   * be filled with default constructed instances of T.
+   * This function will fail if T is not default constructible.
+   */
+  template <size_t DIMEN_TO>
+  Point<DIMEN_TO> orthogonal_projection() const {
+    std::array<T, DIMEN_TO> arr{};
+    arr.fill(T{});
+    // Copy up to current dimension or DIMEN_TO, whichever is smallest
+    std::copy(coordinates_.begin(),
+              coordinates_.begin() + std::min(D, DIMEN_TO), arr.begin());
+    return Point<DIMEN_TO, T>{arr};
+  }
+
+  friend bool operator==(const Point<D>& lhs, const Point<D>& rhs) {
+    return lhs.coordinates_ == rhs.coordinates_;
+  }
+
+  friend bool operator!=(const Point<D>& lhs, const Point<D>& rhs) {
+    return lhs.coordinates_ != rhs.coordinates_;
+  }
+
+ private:
+  std::array<T, D> coordinates_;
 };
 
 /*
  * Finds euclidian distance between two Point classes of same dimension
  */
-template <size_t D>
-double distance(const Point<D>& lhs, const Point<D>& rhs) {
-  double dist = 0;
+template <size_t D, typename T>
+T distance(const Point<D, T>& lhs, const Point<D>& rhs) {
+  T dist{};
   for (size_t i = 0; i < D; i++) dist += std::pow(lhs[i] - rhs[i], 2);
   return std::sqrt(dist);
 }
@@ -41,9 +95,26 @@ double distance(const Point<D>& lhs, const Point<D>& rhs) {
 /*
  * Finds euclidian distance between Point and axis
  */
-template <size_t D>
-double distance(const Point<D>& point, double value, int axis) {
+template <size_t D, typename T>
+T distance(const Point<D, T>& point, const T& value, int axis) {
+  if (axis < 0 && axis > D) throw std::out_of_range{"Invalid axis"};
   return std::abs(point[axis] - value);
+}
+
+/*
+ * Computes manhattan distance of two points that may have different coordinate
+ * storage types.
+ */
+template <size_t D, typename T1, typename T2>
+decltype(T1{} + T2{}) manhattan_distance(const Point<D, T1>& lhs,
+                                         const Point<D, T2>& rhs) {
+  auto sum = decltype(T1{} + T2{}){};
+
+  for (size_t i = 0; i != D; ++i) {
+    sum += std::abs(lhs[i] - rhs[i]);
+  }
+
+  return sum;
 }
 
 /*
@@ -56,13 +127,7 @@ std::ostream& operator<<(std::ostream& os, const Point<D>& point) {
   for (size_t i = 0; i < D - 1; i++) {
     os << point[i] << ", ";
   }
-  os << point[D - 1] << "}";
-  return os;
-}
-
-template <size_t D>
-bool operator==(const Point<D>& lhs, const Point<D>& rhs) {
-  return lhs.coordinates_ == rhs.coordinates_;
+  return os << point[D - 1] << "}";
 }
 
 namespace std {
@@ -72,13 +137,12 @@ namespace std {
 template <size_t D>
 struct hash<Point<D>> {
   size_t operator()(const Point<D>& x) const {
-    /* your code here, e.g. "return hash<int>()(x.value);" */
     return hash<array<double, D>>()(x.coordinates_);
   }
 };
 
 /*
- * Also make it swappable for sorting
+ * Also make it swappable for containers
  */
 template <size_t D>
 void swap(Point<D>& lhs, Point<D>& rhs) {
